9af4d8baa24fcf0923ccc667c9c2571ada759608
amod0017 <amod0017@gmail.com>
2016-05-01 19:32:26 -0500

    Done Some more changes

--- Server/src/edu/lamar/server/Server.java
+++ Server/src/edu/lamar/server/Server.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.lamar.client.message.ClientMessage;
+import edu.lamar.client.message.Operator;
+
 public class Server extends AbstractServer {
 
 	private static final String INVERTED_INDEX_JAR = "/home/hadoop/jar/test.jar";
@@ -52,7 +55,23 @@ public class Server extends AbstractServer {
 
 	@Override
 	protected void handleMessageFromClient(final Object msg, final ConnectionToClient client) {
-
+		final ClientMessage clientMessage = (ClientMessage) msg;
+		if (clientMessage.getOperator() == (Operator.NONE)) {
+			// just get the word with grep command
+			try {
+				String line;
+				String outputToReturn = "";
+				final Process p = Runtime.getRuntime().exec(Server.HADOOP_HOME + " fs -cat " + hadoopOutputPath
+						+ "/*|grep " + clientMessage.getKeyWordsToBeSearched().get(0));
+				final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				while ((line = bufferedReader.readLine()) != null) {
+					outputToReturn = outputToReturn + line;
+				}
+				sendToAllClients(outputToReturn);
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	/**
@@ -68,8 +87,8 @@ public class Server extends AbstractServer {
 
 	public void runInvertedIndex() {
 		try {
-			Runtime.getRuntime()
-					.exec(Server.HADOOP_HOME + " jar" + Server.INVERTED_INDEX_JAR + hadoopInputPath + hadoopOutputPath);
+			Runtime.getRuntime().exec(Server.HADOOP_HOME + " jar " + Server.INVERTED_INDEX_JAR + " " + hadoopInputPath
+					+ " " + hadoopOutputPath);
 			Thread.sleep(2000);
 		} catch (final IOException e) {
 			e.printStackTrace();
