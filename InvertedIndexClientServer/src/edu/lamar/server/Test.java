65c4d954b2ac97d6169a6364827707435813c44f
amod0017 <amod0017@gmail.com>
2016-05-02 19:37:24 -0500

    Updated Server Code

--- InvertedIndexClientServer/src/edu/lamar/server/Test.java
+++ InvertedIndexClientServer/src/edu/lamar/server/Test.java
@@ -1,27 +1,31 @@
 package edu.lamar.server;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		System.out.println("Running jar");
-		try {
-			String line;
-//			Process p = Runtime.getRuntime().exec(
-//					"/usr/local/hadoop/bin/hadoop jar /home/hadoop/jar/test.jar /user/hduser/myinput/input /user/hduser/newoutput");
-			Process p = Runtime.getRuntime().exec(
-					"/usr/local/hadoop/bin/hadoop fs -cat /user/hduser/newoutput/*");
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			while ((line = bufferedReader.readLine()) != null) {
-				System.out.println("not null");
-				System.out.println(line);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		// try {
+		// String line;
+		//// Process p = Runtime.getRuntime().exec(
+		//// "/usr/local/hadoop/bin/hadoop jar /home/hadoop/jar/test.jar
+		// /user/hduser/myinput/input /user/hduser/newoutput");
+		// Process p = Runtime.getRuntime().exec(
+		// "/usr/local/hadoop/bin/hadoop fs -cat /user/hduser/newoutput/*");
+		// BufferedReader bufferedReader = new BufferedReader(new
+		// InputStreamReader(p.getInputStream()));
+		// while ((line = bufferedReader.readLine()) != null) {
+		// System.out.println("not null");
+		// System.out.println(line);
+		// }
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
 		System.out.println("done");
+		final String s = "hadoop->document1,document2";
+		final String[] s1 = s.split("->");
+		System.out.println(Arrays.asList(s1[1].split(",")));
+
 	}
 }
--- Server/src/edu/lamar/server/Server.java
+++ Server/src/edu/lamar/server/Server.java
@@ -1,8 +1,13 @@
 package edu.lamar.server;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import edu.lamar.client.message.ClientMessage;
 import edu.lamar.client.message.Operator;
@@ -32,7 +37,7 @@ public class Server extends AbstractServer {
 
 	public Server(final int port) {
 		super(port);
-		// initialize();
+		initialize();
 		console = new ServerConsole(this);
 
 	}
@@ -43,22 +48,112 @@ public class Server extends AbstractServer {
 		if (clientMessage.getOperator() == (Operator.NONE)) {
 			System.out.println(clientMessage.getKeyWordsToBeSearched());
 			// just get the word with grep command
-			// try {
-			// String line;
-			// String outputToReturn = "";
-			// final Process p = Runtime.getRuntime().exec(Server.HADOOP_HOME +
-			// " fs -cat " + hadoopOutputPath
-			// + "/*|grep " + clientMessage.getKeyWordsToBeSearched().get(0));
-			// final BufferedReader bufferedReader = new BufferedReader(new
-			// InputStreamReader(p.getInputStream()));
-			// while ((line = bufferedReader.readLine()) != null) {
-			// outputToReturn = outputToReturn + line;
-			// }
-			// sendToAllClients(outputToReturn);
-			// } catch (final IOException e) {
-			// e.printStackTrace();
-			// }
+			try {
+				String line;
+				String outputToReturn = "";
+				final Process p = Runtime.getRuntime().exec(Server.HADOOP_HOME + " fs -cat " + hadoopOutputPath
+						+ "/*|grep " + clientMessage.getKeyWordsToBeSearched().get(0));
+				final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				while ((line = bufferedReader.readLine()) != null) {
+					final String[] s1 = line.split("->");
+					outputToReturn = outputToReturn + s1[1];
+				}
+				sendToAllClients(outputToReturn);
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		} else if (clientMessage.getOperator() == Operator.NOT) {
+			try {
+				String line;
+				final List<String> myKeywordContainingList = new ArrayList<String>();
+				final List<String> myOutput = new ArrayList<String>();
+				myOutput.addAll(documentList);
+				final Process p = Runtime.getRuntime().exec(Server.HADOOP_HOME + " fs -cat " + hadoopOutputPath
+						+ "/*|grep " + clientMessage.getKeyWordsToBeSearched().get(0));
+				final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				while ((line = bufferedReader.readLine()) != null) {
+					final String[] s1 = line.split("->");
+					myKeywordContainingList.addAll(Arrays.asList(s1[1].split(",")));
+				}
+				myOutput.removeAll(myKeywordContainingList);
+				sendToAllClients(myOutput.toString());
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		} else if (clientMessage.getOperator() == Operator.OR) {
+			String line;
+			final Set<String> output = new HashSet<String>();
+			try {
+				final Process p1 = Runtime.getRuntime().exec(Server.HADOOP_HOME + " fs -cat " + hadoopOutputPath
+						+ "/*|grep " + clientMessage.getKeyWordsToBeSearched().get(0));
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p1.getInputStream()));
+				while ((line = bufferedReader.readLine()) != null) {
+					final String[] s1 = line.split("->");
+					output.addAll(Arrays.asList(s1[1].split(",")));
+				}
+				final Process p = Runtime.getRuntime().exec(Server.HADOOP_HOME + " fs -cat " + hadoopOutputPath
+						+ "/*|grep " + clientMessage.getKeyWordsToBeSearched().get(1));
+				bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				while ((line = bufferedReader.readLine()) != null) {
+					final String[] s1 = line.split("->");
+					output.addAll(Arrays.asList(s1[1].split(",")));
+				}
+				sendToAllClients(output.toString());
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
+		} else if (clientMessage.getOperator() == Operator.AND) {
+			String line;
+			final Set<String> output1 = new HashSet<String>();
+			final Set<String> output2 = new HashSet<String>();
+			final Set<String> output = new HashSet<String>();
+			try {
+				final Process p1 = Runtime.getRuntime().exec(Server.HADOOP_HOME + " fs -cat " + hadoopOutputPath
+						+ "/*|grep " + clientMessage.getKeyWordsToBeSearched().get(0));
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p1.getInputStream()));
+				while ((line = bufferedReader.readLine()) != null) {
+					final String[] s1 = line.split("->");
+					output1.addAll(Arrays.asList(s1[1].split(",")));
+				}
+				final Process p = Runtime.getRuntime().exec(Server.HADOOP_HOME + " fs -cat " + hadoopOutputPath
+						+ "/*|grep " + clientMessage.getKeyWordsToBeSearched().get(1));
+				bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				while ((line = bufferedReader.readLine()) != null) {
+					final String[] s1 = line.split("->");
+					output2.addAll(Arrays.asList(s1[1].split(",")));
+				}
+				for (final String string : output2) {
+					if (output1.contains(string)) {
+						output.add(string);
+					}
+				}
+				sendToAllClients(output.toString());
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
 		}
+	}
+
+	/**
+	 * This method will initialize the server to get ready for searching
+	 * keywords. Following are the steps that needs to be done: 1. Run inverted
+	 * index. 2. Fetch all the documents name
+	 */
+
+	private void initialize() {
+		runInvertedIndex();
+		try {
+			String line;
+			final Process p = Runtime.getRuntime().exec(
+					Server.HADOOP_HOME + " fs -ls " + hadoopInputPath + " awk \'{print $8}\'" + " sed \'s/.*\\///\'");
+			final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			while ((line = bufferedReader.readLine()) != null) {
+				documentList.add(line);
+			}
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	public void runInvertedIndex() {
